93856881fc66502fe67d9f3ef450371268ff6f5f
brian d foy <briandfoy@pobox.com>
2025-01-06 01:11:51 -0500

    bump copyright year

--- lib/Antsy.pm
+++ lib/Antsy.pm
@@ -922,7 +922,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2021-2024, brian d foy, All Rights Reserved.
+Copyright © 2021-2025, brian d foy, All Rights Reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
